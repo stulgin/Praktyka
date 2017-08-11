5dff1ea300fa5b5d08717ea0bbd5f484cda47599
stulgin <30922548+stulgin@users.noreply.github.com>
2017-08-11 10:46:46 +0200

    Add files via upload

---
+++ demo/DemoApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String args[]) {
+		SpringApplication.run(DemoApplication.class);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Greeting quote = restTemplate.getForObject(
+					"http://localhost:8080/greeting", Greeting.class);
+			log.info(quote.toString());
+		};
+	}
+}
