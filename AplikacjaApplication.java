6b3fa1db20a8760a4f15acc833f3974aba6b8472
stulgin <30922548+stulgin@users.noreply.github.com>
2017-08-11 10:43:21 +0200

    Add files via upload

---
+++ AplikacjaApplication.java
@@ -0,0 +1,12 @@
+package com.example.Aplikacja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikacjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikacjaApplication.class, args);
+	}
+}
